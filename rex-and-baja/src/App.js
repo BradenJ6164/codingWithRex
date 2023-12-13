4fe08385ca56a162ab3eba4a3e26f76ad654846e
RexHell <daleburt01@gmail.com>
2023-12-12 19:00:55 -0800

    Update App.js

--- rex-and-baja/src/App.js
+++ rex-and-baja/src/App.js
@@ -39,7 +39,21 @@ const ListComponent: React.FC = () => {
     </div>
   );
 };
-
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+      </button>
+  );
+}
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
 
 
 // export default App;
